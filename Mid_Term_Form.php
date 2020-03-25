346e22b5d3e6b625266dfc1928785c0204aa4bf9
siamahammad <60424791+siamahammad@users.noreply.github.com>
2020-03-25 16:29:37 +0600

    1st Commit

---
+++ Mid_Term_Form.php
@@ -0,0 +1,66 @@
+<html>
+<head>
+
+</head>
+<body>
+<form>
+
+<table cellpadding="2" width="20%" bgcolor="99FFFF" align="center"
+cellspacing="2">
+
+<tr>
+<td colspan=2>
+<center><font size=4><b>Lab Task Form</b></font></center>
+</td>
+</tr>
+
+<tr>
+<td>Name</td>
+<td><input type=text name=textnames id="textname" size="30"></td>
+</tr>
+
+<tr>
+<td>EmailId</td>
+<td><input type="text" name="emailid" id="emailid" size="30"></td>
+</tr>
+
+
+<tr>
+<td>Gender</td>
+<td><input type="radio" name="gender" value="male" size="10">Male
+<input type="radio" name="gender" value="Female" size="10">Female</td>
+</tr>
+
+<tr>
+<td>DOB</td>
+<td><input type="text" name="dob" id="dob" size="30"></td>
+</tr>
+
+<tr>
+<td>Blood Group</td>
+<td>
+    <input type="radio" id="O+" name="ssc" >O-
+    <input type="radio" id="O-" name="hsc">O+
+    <input type="radio" id="A+"  name="bsc" >A-
+   </td>
+</tr>
+<tr>
+<td>Degree</td>
+<td>
+    <input type="checkbox" id="ssc" name="ssc" >SSC
+    <input type="checkbox" id="hsc" name="hsc">HSC
+    <input type="checkbox" id="bsc"  name="bsc" >BSc
+   </td>
+</tr>
+
+ <tr>
+    <td>Photo</td>
+    <td colspan="2">
+    <input type="file" name="file" value=""> 
+    </td>
+ </tr>
+
+</table>
+</form>
+</body>
+</html>
